30b41a53f2a27d33f342eefa2753e0cfb73e77bc
Nazlıcan <nzlcorak@gmail.com>
2022-06-19 00:31:06 +0300

    İlk ödev gönderim

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " İsimli Müşteriniz Eklendi.");
+        }
+        public void Delete(int Id)
+        {
+            Console.WriteLine(Id + " Id no'lu müşteriniz silindi.");
+        }
+       
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Customer customer1 = new Customer();
+        customer1.Id = 1;
+        customer1.Name = "Nazlı";
+        customer1.LastName = "Çorak";
+
+
+        Customer customer2 = new Customer();
+        customer2.Id = 2;
+        customer2.Name = "Engin";
+        customer2.LastName = "Demiroğ";
+
+
+
+        
+        CustomerManager customerManager = new CustomerManager();
+
+        customerManager.Add(customer1);
+        customerManager.Delete(2);
+      
+    }
+}
